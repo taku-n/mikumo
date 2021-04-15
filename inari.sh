1bd958d477c6ada1219b979904c23646da4c389a
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2021-04-15 18:05:25 +0900

    inari

---
+++ inari.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+cd src
+inari mikumo ../doc
+
+# https://github.com/tkamenoko/inari
+# https://tkamenoko.github.io/inari/
+# https://qiita.com/tkamenoko/items/2991349b4c3c5a668b47
